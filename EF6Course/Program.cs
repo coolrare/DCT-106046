6c2c948fa27cf9da32bc0059bde99f8b54a9444b
Will <doggy.huang@gmail.com>
2017-11-26 14:00:21 +0800

    06 練習透過 Entity Framework 執行 INSERT, UPDATE, DELETE 相關操作

--- EF6Course/Program.cs
+++ EF6Course/Program.cs
@@ -12,12 +12,45 @@ namespace EF6Course
         {
             using (var db = new ContosoUniversityEntities())
             {
-                //db.Database.Log = (sql) => { Console.WriteLine(sql); };
+                db.Database.Log = (sql) => { Console.WriteLine(sql); };
 
                 //GetCourse_Git(db);
                 //GetDepartment(db);
+                //AddCourse(db);
+                //UpdateCourse(db);
+                //DeleteCourse(db);
 
             }
+        }
+
+        private static void DeleteCourse(ContosoUniversityEntities db)
+        {
+            var c = db.Course.Find(9);
+            db.Course.Remove(c);
+            db.SaveChanges();
+        }
+
+        private static void UpdateCourse(ContosoUniversityEntities db)
+        {
+            var items = db.Course.Where(p => p.Title.Contains("Git"));
+            foreach (var item in items)
+            {
+                item.Credits += 1;
+            }
+            db.SaveChanges();
+        }
+
+        private static void AddCourse(ContosoUniversityEntities db)
+        {
+            var c = new Course()
+            {
+                Title = "Entity Framework 6",
+                Credits = 100,
+                IsDeleted = false
+            };
+            c.Department = db.Department.Find(3);
+            db.Course.Add(c);
+            db.SaveChanges();
         }
 
         private static void GetDepartment(ContosoUniversityEntities db)
